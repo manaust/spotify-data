52385ac1d227ae8dafb9d9c32f04ee380a3c16f3
Manaus Transez <mtr@unmatched.eu>
2020-04-13 01:33:38 +0200

    Completed data format script

--- format.js
+++ format.js
@@ -1,5 +1,52 @@
 const fs = require("fs");
-const playlist = JSON.parse(fs.readFileSync("output/output.json"));
-const songs = playlist.tracks.items;
 
-console.log(songs.length);
+const all = [];
+let i = 0;
+
+const writeToFile = (json) => {
+  console.log("Writing json to file...");
+  fs.writeFile(`output/formatted.json`, JSON.stringify(json), "utf8", (err) => {
+    if (err) {
+      console.error("An error occured while writing JSON to file.");
+      return;
+    }
+    console.log("JSON file has been saved.");
+  });
+};
+
+while (true) {
+  try {
+    const tracks = JSON.parse(fs.readFileSync(`output/output${i}.json`));
+    for (let j = 0; j < tracks.length; j++) {
+      let currTrack = tracks[j].track;
+
+      currTrack.artist = currTrack.artists[0].name;
+      currTrack.thumbnails = currTrack.album.images;
+      currTrack.release_date = currTrack.album.release_date;
+      currTrack.album = currTrack.album.name;
+
+      delete currTrack.disc_number;
+      delete currTrack.popularity;
+      delete currTrack.preview_url;
+      delete currTrack.track_number;
+      delete currTrack.video_thumbnail;
+      delete currTrack.available_markets;
+      delete currTrack.track;
+      delete currTrack.type;
+      delete currTrack.episode;
+      delete currTrack.is_local;
+      delete currTrack.external_ids;
+      delete currTrack.external_urls;
+      delete currTrack.uri;
+      delete currTrack.href;
+
+      delete currTrack.artists;
+
+      all.push(currTrack);
+    }
+  } catch (error) {
+    writeToFile(all.sort((a, b) => (a.name > b.name ? 1 : -1)));
+    return;
+  }
+  i++;
+}
